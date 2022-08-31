a6372dcec41fdfce35f5bc925b2af81d1ca982f3
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2022-08-30 22:55:16 -0300

    Criando Classe Menu e Função DrawScreen

---
+++ EditorHtml/Menu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorHtml {
+    internal static class Menu {
+        public static void Show() {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            DrawScreen(10, 30);
+        }
+
+        public static void DrawScreen(int altura, int largura) {
+            TelaLimite(largura);
+            TelaTamanho(altura, largura);
+            TelaLimite(largura);
+        }
+
+        private static void TelaLimite(int largura) {
+            Console.Write("+");
+            for (int i = 0; i <= largura; i++) {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+        }
+
+        private static void TelaTamanho(int altura, int largura) {
+            for (int lines = 0; lines <= altura; lines++) {
+                Console.Write("|");
+                for (int i = 0; i <= largura; i++) {
+                    Console.Write(" ");
+                }
+                Console.Write("|");
+                Console.Write("\n");
+            }
+        }
+    }
+}
--- EditorHtml/Program.cs
+++ EditorHtml/Program.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace EditorHtml {
     internal class Program {
         static void Main(string[] args) {
+            Menu.Show();
+
+            Console.WriteLine();
+            Console.WriteLine("Aperte uma tecla para fechar a aplicação...");
+            Console.ReadKey();
         }
     }
 }
